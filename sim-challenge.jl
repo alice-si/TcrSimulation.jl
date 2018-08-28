1f5d9709f1107eec3c9c1bead34e92d95d12af90
jakub-wojciechowski <jakub@alice.si>
2018-08-28 16:04:23 +0200

    Simulations for summary

--- sim-challenge.jl
+++ sim-challenge.jl
@@ -22,13 +22,15 @@ function runSimulation(num_of_steps, num_of_agents, accuracy)
 end
 
 
+mean([runSimulation(1000, 100, acc) for i in 1:10])
+
 
 #Run scenario (the impact of accuracy on list quality, 10 iterations)
-for acc in 0:10:100
-    score = mean([runSimulation(1500, 100, acc) for i in 1:10])
-    #println("Accuracy: $acc Score: $score")
-    println("$score")
-end
+# for acc in 0:10:100
+#     score = mean([runSimulation(1000, 100, acc) for i in 1:10])
+#     #println("Accuracy: $acc Score: $score")
+#     println("$score")
+# end
 
 
 
