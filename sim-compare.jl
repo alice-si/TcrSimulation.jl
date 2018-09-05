d2b40ba1a23bf576855b2889c796b512ade9f59a
jakub-wojciechowski <jakub@alice.si>
2018-09-05 10:17:56 +0200

    Additional scenarios

--- sim-compare.jl
+++ sim-compare.jl
@@ -1,6 +1,24 @@
 
+#Accuracy -> efficiency
 for acc in 0:2:100
-    noToken = mean([highDiversityWithChallenge(1000, 50, acc, 10, Benchmarks.registryMean) for i in 1:20])
-    token = mean([binaryTokenChallenge(1000, 50, acc, 10, Benchmarks.registryMean) for i in 1:20])
+    noToken = mean([diversityWithChallenge(1000, 50, acc, 10, [Benchmarks.registryMean]) for i in 1:20])[1]
+    token = mean([binaryTokenChallenge(1000, 50, acc, 10, [Benchmarks.registryMean]) for i in 1:20])[1]
     println("$acc, $noToken, $token")
+end
+
+
+
+#Agents of steps -> efficiency
+for agents in 10:200:2000
+    noToken = mean([diversityWithChallenge(1000, agents, 50, 20, [Benchmarks.registryMean]) for i in 1:20])[1]
+    token = mean([binaryTokenChallenge(1000, agents, 50, 20, [Benchmarks.registryMean]) for i in 1:20])[1]
+    println("$agents, $noToken, $token")
+end
+
+
+#Number of steps -> efficiency
+for steps in 100:100:2000
+    noToken = mean([diversityWithChallenge(steps, 50, 50, 20, [Benchmarks.registryMean]) for i in 1:20])[1]
+    token = mean([binaryTokenChallenge(steps, 50, 50, 20, [Benchmarks.registryMean]) for i in 1:20])[1]
+    println("$steps, $noToken, $token")
 end
---
+++ sim-explain.jl
@@ -0,0 +1,9 @@
+
+
+
+for acc in 0:2:100
+    results = mean([binaryTokenChallenge(1000, 50, acc, 20, [Benchmarks.accuracyBoost, Benchmarks.accuracyBalanceCorrelation]) for i in 1:40])
+    boost = results[2]
+    correlation = results[3]
+    println("$acc, $boost, $correlation")
+end
